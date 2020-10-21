9d33eac9bc0ce7e5d3d692afc34b4afb20889425
lorenzo pisaneschi <lorenzo.pisaneschi@gmail.com>
2020-10-21 17:25:32 +0200

    typo fix

--- com.unifi.attws.exam.museum-app/src/main/java/com/unifi/attsw/exam/transaction/manager/RepositoryFunction.java
+++
@@ -1,7 +0,0 @@
-package com.unifi.attws.exam.transaction.manager;
-
-@FunctionalInterface
-public interface RepositoryFunction<MuseumRepository, ExhibitionRepository, T> {
-	public T apply(MuseumRepository museumRepository, ExhibitionRepository exhibitionRepository);
-
-}
--- com.unifi.attws.exam.museum-app/src/main/java/com/unifi/attsw/exam/transaction/manager/TransactionManager.java
+++
@@ -1,8 +0,0 @@
-package com.unifi.attws.exam.transaction.manager;
-
-import com.unifi.attws.exam.exception.RepositoryException;
-
-public interface TransactionManager {
-	<T> T doInTransaction(TransactionCode<T> code) throws RepositoryException;
-
-}
