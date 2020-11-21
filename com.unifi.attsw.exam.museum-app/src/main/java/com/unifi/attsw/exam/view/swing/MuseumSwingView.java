c91bec80d84ad4152d5e8539c3bcb2dae7fde52e
lorenzo pisaneschi <lorenzo.pisaneschi@gmail.com>
2020-11-21 11:52:50 +0100

    test enable add button

--- com.unifi.attsw.exam.museum-app/src/main/java/com/unifi/attsw/exam/view/swing/MuseumSwingView.java
+++ com.unifi.attsw.exam.museum-app/src/main/java/com/unifi/attsw/exam/view/swing/MuseumSwingView.java
@@ -14,8 +14,11 @@ import java.awt.Insets;
 import javax.swing.JButton;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
+import java.awt.Color;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
-public class MuseumSwingView extends JFrame implements MuseumView{
+public class MuseumSwingView extends JFrame implements MuseumView {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
@@ -33,12 +36,12 @@ public class MuseumSwingView extends JFrame implements MuseumView{
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[] {100, 100, 152, 100};
-		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_contentPane.columnWeights = new double[]{1.0, 1.0, 1.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWidths = new int[] { 100, 100, 152, 100 };
+		gbl_contentPane.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		gbl_contentPane.columnWeights = new double[] { 1.0, 1.0, 1.0, Double.MIN_VALUE };
+		gbl_contentPane.rowWeights = new double[] { 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
 		contentPane.setLayout(gbl_contentPane);
-		
+
 		JLabel lblMuseum = new JLabel("Museum");
 		GridBagConstraints gbc_lblMuseum = new GridBagConstraints();
 		gbc_lblMuseum.anchor = GridBagConstraints.EAST;
@@ -46,9 +49,9 @@ public class MuseumSwingView extends JFrame implements MuseumView{
 		gbc_lblMuseum.gridx = 0;
 		gbc_lblMuseum.gridy = 0;
 		contentPane.add(lblMuseum, gbc_lblMuseum);
-		
+
 		txtMuseum = new JTextField();
-		txtMuseum.setText("museum");
+		txtMuseum.setName("museum");
 		GridBagConstraints gbc_txtMuseum = new GridBagConstraints();
 		gbc_txtMuseum.insets = new Insets(0, 0, 5, 5);
 		gbc_txtMuseum.fill = GridBagConstraints.HORIZONTAL;
@@ -56,14 +59,14 @@ public class MuseumSwingView extends JFrame implements MuseumView{
 		gbc_txtMuseum.gridy = 0;
 		contentPane.add(txtMuseum, gbc_txtMuseum);
 		txtMuseum.setColumns(10);
-		
+
 		JButton btnFindAll = new JButton("Find all");
 		GridBagConstraints gbc_btnFindAll = new GridBagConstraints();
 		gbc_btnFindAll.insets = new Insets(0, 0, 5, 0);
 		gbc_btnFindAll.gridx = 3;
 		gbc_btnFindAll.gridy = 0;
 		contentPane.add(btnFindAll, gbc_btnFindAll);
-		
+
 		JLabel lblRooms = new JLabel("Rooms");
 		GridBagConstraints gbc_lblRooms = new GridBagConstraints();
 		gbc_lblRooms.anchor = GridBagConstraints.EAST;
@@ -71,9 +74,9 @@ public class MuseumSwingView extends JFrame implements MuseumView{
 		gbc_lblRooms.gridx = 0;
 		gbc_lblRooms.gridy = 1;
 		contentPane.add(lblRooms, gbc_lblRooms);
-		
+
 		txtRooms = new JTextField();
-		txtRooms.setText("rooms");
+		txtRooms.setName("rooms");
 		GridBagConstraints gbc_txtRooms = new GridBagConstraints();
 		gbc_txtRooms.insets = new Insets(0, 0, 5, 5);
 		gbc_txtRooms.fill = GridBagConstraints.HORIZONTAL;
@@ -81,15 +84,32 @@ public class MuseumSwingView extends JFrame implements MuseumView{
 		gbc_txtRooms.gridy = 1;
 		contentPane.add(txtRooms, gbc_txtRooms);
 		txtRooms.setColumns(10);
-		
+
 		JButton btnAdd = new JButton("Add");
+		KeyAdapter btnAddEnabler = new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+				btnAdd.setEnabled(!txtMuseum.getText().isEmpty() && !txtMuseum.getText().isEmpty());
+			}
+		}
+		;
+		btnAdd.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+				btnAdd.setEnabled(!txtMuseum.getText().isEmpty() && !txtMuseum.getText().isEmpty());
+			}
+		});
 		btnAdd.setEnabled(false);
+		
+		txtRooms.addKeyListener(btnAddEnabler);
+		btnAdd.setEnabled(false);
+		
 		GridBagConstraints gbc_btnAdd = new GridBagConstraints();
 		gbc_btnAdd.insets = new Insets(0, 0, 5, 5);
 		gbc_btnAdd.gridx = 1;
 		gbc_btnAdd.gridy = 2;
 		contentPane.add(btnAdd, gbc_btnAdd);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
 		gbc_scrollPane.gridwidth = 4;
@@ -98,19 +118,21 @@ public class MuseumSwingView extends JFrame implements MuseumView{
 		gbc_scrollPane.gridx = 0;
 		gbc_scrollPane.gridy = 3;
 		contentPane.add(scrollPane, gbc_scrollPane);
-		
+
 		JList museumList = new JList();
 		museumList.setName("museumList");
 		scrollPane.setViewportView(museumList);
-		
-		JLabel lblNewLabel = new JLabel("errorMessageLabel");
-		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-		gbc_lblNewLabel.gridwidth = 3;
-		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel.gridx = 0;
-		gbc_lblNewLabel.gridy = 5;
-		contentPane.add(lblNewLabel, gbc_lblNewLabel);
-		
+
+		JLabel errorMessageLabel = new JLabel(" ");
+		errorMessageLabel.setForeground(Color.RED);
+		errorMessageLabel.setName("errorMessageLabel");
+		GridBagConstraints gbc_errorMessageLabel = new GridBagConstraints();
+		gbc_errorMessageLabel.gridwidth = 3;
+		gbc_errorMessageLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_errorMessageLabel.gridx = 0;
+		gbc_errorMessageLabel.gridy = 5;
+		contentPane.add(errorMessageLabel, gbc_errorMessageLabel);
+
 		JButton btnDeleteSelected = new JButton("Delete Selected");
 		btnDeleteSelected.setEnabled(false);
 		GridBagConstraints gbc_btnDeleteSelected = new GridBagConstraints();
@@ -124,7 +146,7 @@ public class MuseumSwingView extends JFrame implements MuseumView{
 	@Override
 	public void showError(String message, Museum museum) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
--- com.unifi.attsw.exam.museum-app/src/test/java/com/unifi/attsw/exam/test/view/swing/MuseumSwingViewTest.java
+++ com.unifi.attsw.exam.museum-app/src/test/java/com/unifi/attsw/exam/test/view/swing/MuseumSwingViewTest.java
@@ -2,7 +2,6 @@ package com.unifi.attsw.exam.test.view.swing;
 
 import org.assertj.swing.core.matcher.JButtonMatcher;
 import org.assertj.swing.core.matcher.JLabelMatcher;
-import org.assertj.swing.core.matcher.JTextComponentMatcher;
 import org.assertj.swing.edt.GuiActionRunner;
 import org.assertj.swing.fixture.FrameFixture;
 import org.assertj.swing.junit.runner.GUITestRunner;
@@ -13,10 +12,13 @@ import org.junit.runner.RunWith;
 import com.unifi.attsw.exam.view.swing.MuseumSwingView;
 
 @RunWith(GUITestRunner.class)
-public class MuseumSwingViewTest extends AssertJSwingJUnitTestCase{
-	
+public class MuseumSwingViewTest extends AssertJSwingJUnitTestCase {
+
+	private static final String MUSEUM1_TEST = "museum1_test";
+	private static final String NUM_CONST = "10";
+
 	private FrameFixture window;
-	
+
 	private MuseumSwingView museumSwingView;
 
 	@Override
@@ -27,19 +29,26 @@ public class MuseumSwingViewTest extends AssertJSwingJUnitTestCase{
 		});
 		window = new FrameFixture(robot(), museumSwingView);
 		window.show();
-		
 	}
-	
+
 	@Test
 	public void testMuseumDashboardControlsInitialState() {
 		window.label(JLabelMatcher.withText("Museum"));
-		window.textBox(JTextComponentMatcher.withText("museum")).requireEnabled();
+		window.textBox("museum").requireEnabled();
 		window.label(JLabelMatcher.withText("Rooms"));
-		window.textBox(JTextComponentMatcher.withText("rooms")).requireEnabled();
+		window.textBox("rooms").requireEnabled();
 		window.button(JButtonMatcher.withText("Add")).requireDisabled();
 		window.button(JButtonMatcher.withText("Find all")).requireEnabled();
 		window.list("museumList");
 		window.button(JButtonMatcher.withText("Delete Selected")).requireDisabled();
+		window.label("errorMessageLabel").requireText(" ");
+	}
+
+	@Test
+	public void testWhenMuseumNameAndRoomsAreFilledAddButtonShouldBeEnabled() {
+		window.textBox("museum").enterText(MUSEUM1_TEST);
+		window.textBox("rooms").enterText(NUM_CONST);
+		window.button(JButtonMatcher.withText("Add")).requireEnabled();
 	}
 
 }
