cddb8eebf4e7f34964af9fdac0b2fb0f3d818ae5
lorenzo pisaneschi <lorenzo.pisaneschi@gmail.com>
2020-12-04 16:25:29 +0100

    Jacoco fix and setup for e2e tests

---
+++ com.unifi.attsw.exam.museum-app/src/e2e/java/com/unifi/attsw/exam/e2e/swing/app/MuseumSwingAppE2E.java
@@ -0,0 +1,17 @@
+package com.unifi.attsw.exam.e2e.swing.app;
+
+import static org.junit.Assert.*;
+
+import org.assertj.swing.junit.runner.GUITestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(GUITestRunner.class)
+public class MuseumSwingAppE2E {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
