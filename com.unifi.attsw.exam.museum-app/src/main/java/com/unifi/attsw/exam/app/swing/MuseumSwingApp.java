24ab4ff8e6d72bbd5b58316bea768a7842658bbc
lorenzo pisaneschi <lorenzo.pisaneschi@gmail.com>
2020-12-05 13:00:39 +0100

    Added hikari cp

--- com.unifi.attsw.exam.museum-app/src/main/java/com/unifi/attsw/exam/app/swing/MuseumSwingApp.java
+++ com.unifi.attsw.exam.museum-app/src/main/java/com/unifi/attsw/exam/app/swing/MuseumSwingApp.java
@@ -1,11 +1,15 @@
 package com.unifi.attsw.exam.app.swing;
 
 import java.awt.EventQueue;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Callable;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+
+import org.apache.logging.log4j.core.tools.picocli.CommandLine.Option;
 
 import com.unifi.attsw.exam.controller.MuseumController;
 import com.unifi.attsw.exam.service.MuseumManagerService;
@@ -20,16 +24,30 @@ import picocli.CommandLine.Command;
 
 @Command(mixinStandardHelpOptions = true)
 public class MuseumSwingApp implements Callable<Void> {
-	
+
+	@Option(names = { "--db-url" }, description = "Database url connection.")
+	private String databaseUrl = "jdbc:postgresql://localhost:5432/ATTSW_DB";
+
+	@Option(names = { "--db-user" }, description = "Database user.")
+	private String databaseUser = "museum_manager";
+
+	@Option(names = { "--db-password" }, description = "Database password.")
+	private String databasePassword = "attsw";
+
 	public static void main(String[] args) {
-		// TODO: add commands
 		new CommandLine(new MuseumSwingApp()).execute(args);
 	}
 
 	@Override
 	public Void call() throws Exception {
 		EventQueue.invokeLater(() -> {
-			EntityManagerFactory sessionFactory = Persistence.createEntityManagerFactory("real.postgres");
+
+			Map<String, String> properties = new HashMap<>();
+			properties.put("javax.persistence.jdbc.url", databaseUrl);
+			properties.put("javax.persistence.jdbc.user", databaseUser);
+			properties.put("javax.persistence.jdbc.password", databasePassword);
+
+			EntityManagerFactory sessionFactory = Persistence.createEntityManagerFactory("real.postgres", properties);
 			EntityManager entityManager = sessionFactory.createEntityManager();
 			TransactionManager transactionManager = new PostgresTransactionManager(entityManager);
 			MuseumManagerService museumManagerService = new MuseumManagerServiceImpl(transactionManager);
